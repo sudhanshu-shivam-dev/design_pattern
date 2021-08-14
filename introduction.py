607afad72037730abbc91d9a6e5fed25175410b9
sudhanshu shivam <shivam.sudhanshu.dev@gmail.com>
2021-08-14 13:36:20 +0530

    initial commit

---
+++ introduction.py
@@ -0,0 +1,9 @@
+'''
+    This code in the book is about encaspulation
+    Encapsulation is used:
+        1) Hiding Data 
+        2) Simplifying Behaviour
+    we encapsulate behaviour by identifying a task that needs to be done in our code and giving that task to a well-defined 
+    object or function. We call the object that object or function an abstraction 
+'''
+
